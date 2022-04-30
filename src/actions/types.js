d582c6abd4b720249e2cc1b8db63ab68d2817a62
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-04-30 13:16:12 +0100

    ft-setup-action-types

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNOUT = 'SIGNOUT';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNIN_FAILURE = 'SIGNIN_FAILURE';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+export const SET_MESSAGE = 'SET_MESSAGE';
+export const CLEAR_MESSAGE = 'CLEAR_MESSAGE';
